3bf1d56c9b99e5d2caf98049af051e6d9914685b
Evans Chen <evansschen@gmail.com>
2025-07-13 10:22:25 -0700

    Fix DataEntryValue hooks to use ControlFlowPrefix

--- src/PEAKLib.Items/Hooks/DataEntryValueHooks.cs
+++ src/PEAKLib.Items/Hooks/DataEntryValueHooks.cs
@@ -1,13 +1,13 @@
 ﻿#if !UNITY_EDITOR
 using System;
-using System.Collections.Generic;
 using MonoDetour;
+using MonoDetour.DetourTypes;
 using MonoDetour.HookGen;
 using On.DataEntryValue;
 
 namespace PEAKLib.Items.Hooks;
 
-[MonoDetourTargets(typeof(DataEntryValue))]
+[MonoDetourTargets(typeof(DataEntryValue), GenerateControlFlowVariants = true)]
 static class DataEntryValueHooks
 {
     public const byte PEAKLIB_MOD_TYPE_INDEX = 42;
@@ -16,8 +16,7 @@ static class DataEntryValueHooks
     static void Init()
     {
         GetTypeValue.Postfix(Postfix_GetTypeValue);
-        GetNewFromValue.Prefix(Prefix_GetNewFromValue);
-        GetNewFromValue.Postfix(Postfix_GetNewFromValue);
+        GetNewFromValue.ControlFlowPrefix(Prefix_GetNewFromValue);
     }
 
     static void Postfix_GetTypeValue(ref Type type, ref byte returnValue)
@@ -28,27 +27,14 @@ static class DataEntryValueHooks
         }
     }
 
-    // unlike Harmony, there doesn't appear to be a way to suppress the call after a prefix with MonoDetour
-    // so we need this horrid hack tracking (threadId -> arg)
-    // otherwise GetNewFromValue will throw
-    static Dictionary<int, byte> Args_GetNewFromValue = new Dictionary<int, byte>();
-    static void Prefix_GetNewFromValue(ref byte value)
+    static ReturnFlow Prefix_GetNewFromValue(ref byte value, ref DataEntryValue returnValue)
     {
         if (value == PEAKLIB_MOD_TYPE_INDEX)
         {
-            Args_GetNewFromValue[Environment.CurrentManagedThreadId] = value;
-            value = 1;
-        }
-    }
-    static void Postfix_GetNewFromValue(ref byte value, ref DataEntryValue returnValue)
-    {
-        int thread = Environment.CurrentManagedThreadId;
-        if (Args_GetNewFromValue.ContainsKey(thread))
-        {
-            value = Args_GetNewFromValue[thread];
-            Args_GetNewFromValue.Remove(thread);
             returnValue = new ModItemData();
+            return ReturnFlow.SkipOriginal;
         }
+        return ReturnFlow.None;
     }
 }
 #endif
