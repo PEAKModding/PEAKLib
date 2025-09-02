0abc431204a46a55b71e2870960ba124159e4e22
Hamunii <samuel.suni04@gmail.com>
2025-09-03 01:03:15 +0300

    add empty PEAKLib.Levels submodule

---
+++ src/PEAKLib.Levels/Plugin.cs
@@ -0,0 +1,25 @@
+﻿#if !UNITY_EDITOR
+using BepInEx;
+using BepInEx.Logging;
+using MonoDetour;
+using PEAKLib.Core;
+
+namespace PEAKLib.Levels;
+
+/// <summary>
+/// BepInEx plugin of PEAKLib.Levels.
+/// Depend on this with <c>[BepInDependency(LevelsPlugin.Id)]</c>.
+/// </summary>
+[BepInAutoPlugin]
+[BepInDependency(CorePlugin.Id)]
+public partial class LevelsPlugin : BaseUnityPlugin
+{
+    internal static ManualLogSource Log { get; } = BepInEx.Logging.Logger.CreateLogSource(Name);
+
+    private void Awake()
+    {
+        MonoDetourManager.InvokeHookInitializers(typeof(LevelsPlugin).Assembly);
+        Log.LogInfo($"Plugin {Name} is loaded!");
+    }
+}
+#endif
