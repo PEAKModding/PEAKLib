04d5f5e78dd1f9b4e2d2883fa4db91035f7f9381
MonsieurMeh <mensoirmeh@gmail.com>
2025-12-12 11:30:55 -0800

    feat: ShaderExtensions class with transferred code from ItemLib

---
+++ src/PEAKLib.Core/Extensions/ShaderExtensions.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace PEAKLib.Core.Extensions;
+
+internal static class ShaderExtensions
+{
+    private static Dictionary<string, Shader> _peakShaders = null!;
+    internal static Dictionary<string, Shader> PeakShaders
+    {
+        get
+        {
+            if (_peakShaders == null)
+            {
+                var shaders = new List<string>
+                {
+                    "W/Peak_Standard",
+                    "W/Character",
+                    "W/Peak_Transparent",
+                    "W/Peak_Glass",
+                    "W/Peak_Clip",
+                    "W/Peak_glass_liquid",
+                    "W/Peak_GroundTransition",
+                    "W/Peak_Guidebook",
+                    "W/Peak_Honey",
+                    "W/Peak_Ice",
+                    "W/Peak_Rock",
+                    "W/Peak_Rope",
+                    "W/Peak_Splash",
+                    "W/Peak_Waterfall",
+                    "W/Vine",
+                };
+                _peakShaders = new();
+                foreach (var sh in shaders)
+                {
+                    var shader = Shader.Find(sh);
+                    if (shader == null)
+                    {
+                        CorePlugin.Log.LogWarning(
+                            $"{nameof(PeakShaders)}: Shader {sh} was not found."
+                        );
+                        continue;
+                    }
+                    _peakShaders[sh] = shader;
+                }
+            }
+            return _peakShaders;
+        }
+    }
+}
+
