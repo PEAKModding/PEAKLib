55bcd6fc021d7fe9761d8bf1126fe1d39e5c3f74
MonsieurMeh <mensoirmeh@gmail.com>
2025-12-12 12:00:56 -0800

    feat: IComponentContent for core-level component operations like shader replacement

---
+++ src/PEAKLib.Core/IComponentContent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace PEAKLib.Core;
+
+internal interface IComponentContent
+{
+    Component Component { get; }
+}
