60745585e2f3e3e34dc0f409b48eaa21b939f7a5
MonsieurMeh <mensoirmeh@gmail.com>
2025-12-12 12:22:18 -0800

    feat: Implement component-oriented IComponentContent interface  for related operations within corelib.

--- src/PEAKLib.Core/IComponentContent.cs
+++ src/PEAKLib.Core/IComponentContent.cs
@@ -2,7 +2,13 @@
 
 namespace PEAKLib.Core;
 
-internal interface IComponentContent
+/// <summary>
+/// A component-focused interface for a registrable mod content allowing component-level operations within core lib.
+/// </summary>
+public interface IComponentContent
 {
+    /// <summary>
+    /// Link to primary component associated with content. 
+    /// </summary>
     Component Component { get; }
 }
